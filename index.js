5cd12803b6403515da1616681f0a0ba275f7ed6e
bug3 <bug3dev@gmail.com>
2024-01-10 04:24:11 +0300

    Export convertQRCode module

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const decodeMigrationURI = require('./src/decodeMigrationURI');
 const syncPromise = require('synchronized-promise');
+const decodeMigrationURI = require('./src/decodeMigrationURI');
+const convertQRCode = require('./src/convertQRCode');
 
 const decodeMigrationURISync = (migrationURI) => syncPromise(decodeMigrationURI)(migrationURI);
+const convertQRCodeSync = (qrImageFile) => syncPromise(convertQRCode)(qrImageFile);
 
 module.exports.decodeMigrationURI = decodeMigrationURI;
 module.exports.decodeMigrationURISync = decodeMigrationURISync;
+module.exports.convertQRCode = convertQRCode;
+module.exports.convertQRCodeSync = convertQRCodeSync;
