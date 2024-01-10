be696f0ff217c7fd3b1868d3a354d149675ffef3
bug3 <bug3dev@gmail.com>
2024-01-10 03:23:34 +0300

    Catch errors

--- src/decodeMigrationURI.js
+++ src/decodeMigrationURI.js
@@ -4,29 +4,33 @@ const protoFile = 'migration_data.proto';
 
 module.exports = async (uri) => await new Promise((resolve, reject) => {
     protobuf.load(protoFile, (loadError, root) => {
-        if (typeof uri !== 'string' || !uri.includes('data')) {
-            reject('Invalid URI: Data parameter not found');
+        try {
+            if (typeof uri !== 'string' || !uri.includes('data')) {
+                reject('Invalid URI: Data parameter not found');
+            }
+
+            if (loadError) {
+                reject(loadError);
+            }
+
+            const message = root.lookupType('otpauth.MigrationData');
+            const payload = Buffer.from(new URL(uri).searchParams.get('data'), 'base64');
+            const verifyError = message.verify(payload);
+
+            if (verifyError) {
+                reject(verifyError);
+            }
+
+            const decodedMessage = message.decode(payload);
+            const messageObject = message.toObject(decodedMessage, {
+                longs: String,
+                enums: String,
+                bytes: String
+            });
+
+            resolve(messageObject.otpParams);
+        } catch (error) {
+            reject(error);
         }
-
-        if (loadError) {
-            reject(loadError);
-        }
-
-        const message = root.lookupType('otpauth.MigrationData');
-        const payload = Buffer.from(new URL(uri).searchParams.get('data'), 'base64');
-        const verifyError = message.verify(payload);
-
-        if (verifyError) {
-            reject(verifyError);
-        }
-
-        const decodedMessage = message.decode(payload);
-        const messageObject = message.toObject(decodedMessage, {
-            longs: String,
-            enums: String,
-            bytes: String
-        });
-
-        resolve(messageObject.otpParams);
     });
 });
