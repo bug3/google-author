defc7f3ae4ca41510184f2a5f63c03266ed62c97
bug3 <bug3dev@gmail.com>
2024-01-10 03:29:57 +0300

    Add sync support for decodeMigrationURI

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const decodeMigrationURI = require('./src/decodeMigrationURI');
+const syncPromise = require('synchronized-promise');
 
 const uri = 'otpauth-migration://offline?data=';
 
 decodeMigrationURI(uri)
     .then((data) => console.log(data))
-    .catch((err) => console.log(err));
+    .catch((error) => console.log(error));
+
+console.log(syncPromise(decodeMigrationURI)(uri));
