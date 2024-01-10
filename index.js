76929d978361335490304392d5f5213cc229be19
bug3 <bug3dev@gmail.com>
2024-01-10 03:16:03 +0300

    Create decodeMigrationURI.js

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
-const protobuf = require('protobufjs');
+const decodeMigrationURI = require('./src/decodeMigrationURI');
 
-const protoFile = 'migration_data.proto';
 const uri = 'otpauth-migration://offline?data=';
 
-if (typeof uri !== 'string' || !uri.includes('data')) {
-    throw Error('Invalid URI: Data parameter not found');
-}
-
-protobuf.load(protoFile, (loadError, root) => {
-    if (loadError) {
-        throw loadError;
-    }
-
-    const message = root.lookupType('otpauth.MigrationData');
-    const payload = Buffer.from(new URL(uri).searchParams.get('data'), 'base64');
-    const verifyError = message.verify(payload);
-
-    if (verifyError) {
-        throw Error(verifyError);
-    }
-
-    const decodedMessage = message.decode(payload);
-    const messageObject = message.toObject(decodedMessage, {
-        longs: String,
-        enums: String,
-        bytes: String
-    });
-
-    console.log(messageObject.otpParams);
-});
+decodeMigrationURI(uri)
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
